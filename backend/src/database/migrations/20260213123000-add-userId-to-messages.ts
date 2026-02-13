e636d9422f856e73470e17ed5f0d2c593cde5425
Osmar F Cavalcante <osmarnube@gmail.com>
2026-02-13 09:32:13 -0300

    chore: convert migration to ts to ensure build

--- backend/src/database/migrations/20260213123000-add-userId-to-messages.ts
+++ backend/src/database/migrations/20260213123000-add-userId-to-messages.ts
@@ -1,9 +1,9 @@
-'use strict';
+import { QueryInterface, DataTypes } from "sequelize";
 
 module.exports = {
-    up: (queryInterface, Sequelize) => {
+    up: (queryInterface: QueryInterface) => {
         return queryInterface.addColumn("Messages", "userId", {
-            type: Sequelize.INTEGER,
+            type: DataTypes.INTEGER,
             references: { model: "Users", key: "id" },
             onUpdate: "CASCADE",
             onDelete: "SET NULL",
@@ -11,7 +11,7 @@ module.exports = {
         });
     },
 
-    down: (queryInterface, Sequelize) => {
+    down: (queryInterface: QueryInterface) => {
         return queryInterface.removeColumn("Messages", "userId");
     }
 };
